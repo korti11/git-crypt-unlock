c56e5105094a41307230627dfac7438230a02a83
Korti <lucas.korthauer@gmail.com>
2019-12-17 13:59:13 +0100

    Deleted wait.js. Changed index.js to unlock with git-crypt under macos.

--- index.js
+++ index.js
@@ -1,18 +1,30 @@
 const core = require('@actions/core');
-const wait = require('./wait');
-
+const exec = require('@actions/exec');
+const os = require('os')
 
 // most @actions toolkit packages have async methods
 async function run() {
   try { 
-    const ms = core.getInput('milliseconds');
-    console.log(`Waiting ${ms} milliseconds ...`)
+    const key = core.getInput('GIT_CRYPT_KEY');
+    const osType = os.type();
 
-    core.debug((new Date()).toTimeString())
-    wait(parseInt(ms));
-    core.debug((new Date()).toTimeString())
+    switch(osType) {
+      case 'Darwin':
+        exec.exec('brew install git-crypt');
+        break;
+      case 'Linux':
+        throw new Error('Linux currently not supported.');
+      case 'Windows_NT':
+        throw new Error('Windows currently not supported.');
+      default:
+        // Should never be thrown on github workflows.
+        throw new Error(`OS: ${osType} not supported. What did you do this should never happened :O`);
+    }
 
-    core.setOutput('time', new Date().toTimeString());
+    exec.exec(`cat ${key} > ./tmp-base64.key && base64 -d ./tmp-base64.key > ./secret-key.key`);
+    exec.exec('git-crypt unlock ./secrete-key.key');
+
+    core.info('Secrets unlocked.');
   } 
   catch (error) {
     core.setFailed(error.message);
