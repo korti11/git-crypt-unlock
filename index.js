b1f428370d089145d2cf9d6edfe6688983b48d02
Korti <lucas.korthauer@gmail.com>
2020-03-03 21:01:06 +0100

    Changed the file destination.

--- index.js
+++ index.js
@@ -26,7 +26,8 @@ async function run() {
         await exec.exec('sudo apt-get install -y git-crypt');
         break;
       case 'Windows_NT':
-        const gitCryptPath = await tc.downloadTool('https://github.com/oholovko/git-crypt-windows/releases/download/1.0.35/git-crypt.exe', './git-crypt.exe');
+        const currentPath = process.cwd();
+        const gitCryptPath = await tc.downloadTool('https://github.com/oholovko/git-crypt-windows/releases/download/1.0.35/git-crypt.exe', `${currentPath}\\git-crypt.exe`);
         core.info(`Git crypt path ${gitCryptPath}`);
         break;
       default:
